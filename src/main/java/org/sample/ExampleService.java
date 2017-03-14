a25cdc418daaa67bf3ee80d0d7366b6f5dbb71e7
Shubham Vashisht <vashisht.s21295@gmail.com>
2017-03-14 23:53:38 +0530

    files added

---
+++ src/main/java/org/sample/ExampleService.java
@@ -0,0 +1,8 @@
+package org.sample;
+
+/**
+ * Created by Droidman on 3/14/2017.
+ */
+public interface ExampleService {
+    public void doSomething();
+}
---
+++ src/main/java/org/sample/ExampleServiceImpl.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+/**
+ * Created by Droidman on 3/14/2017.
+ */
+public class ExampleServiceImpl implements ExampleService {
+    @java.lang.Override
+    public void doSomething() {
+
+    }
+}
